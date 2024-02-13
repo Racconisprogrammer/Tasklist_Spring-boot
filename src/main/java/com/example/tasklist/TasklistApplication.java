c1e4a14bfd268c9e68c27f185348e543a31f91b3
Farkhod <13frasx@gmail.com>
2024-02-13 07:17:46 -0800

    Modified package service

--- src/main/java/com/example/tasklist/TasklistApplication.java
+++ src/main/java/com/example/tasklist/TasklistApplication.java
@@ -4,8 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableTransactionManagement
 public class TasklistApplication {
 
 	public static void main(String[] args) {
