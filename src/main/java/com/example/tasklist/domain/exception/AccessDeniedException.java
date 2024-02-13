4abfddcf523f2b3f131dd5b31db9551801910764
Farkhod <13frasx@gmail.com>
2024-02-13 10:36:25 -0800

    Added functionality JWT tokens

--- src/main/java/com/example/tasklist/domain/exception/AccessDeniedException.java
+++ src/main/java/com/example/tasklist/domain/exception/AccessDeniedException.java
@@ -2,7 +2,7 @@ package com.example.tasklist.domain.exception;
 
 public class AccessDeniedException extends RuntimeException {
 
-    public AccessDeniedException(String message) {
-        super(message);
+    public AccessDeniedException() {
+        super();
     }
 }
---
+++ src/main/java/com/example/tasklist/service/props/JwtProperties.java
@@ -0,0 +1,18 @@
+package com.example.tasklist.service.props;
+
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@Data
+@ConfigurationProperties(prefix = "security.jwt")
+public class JwtProperties {
+
+    private String secret;
+    private Long access;
+    private Long refresh;
+
+
+}
